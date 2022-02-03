66324f4c749e976375dff9480432ed62153d7cba
kishoretheeraj <kishoretheeraj@gmail.com>
2022-02-03 20:32:50 +0530

    seventh commit

--- app/src/main/java/com/app/madrasdefenceacademy/MainActivity.java
+++ app/src/main/java/com/app/madrasdefenceacademy/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Dhanush
+        //feature 3
+        //feature 4
     }
 }
